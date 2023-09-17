41431c0c1ac4141c8b683b3c35f12b76217b4728
Celço Neto <celconeto@gmail.com>
2023-09-17 12:15:13 -0300

    feat: create api of upload ai

---
+++ src/routes/get-all-prompts.ts
@@ -0,0 +1,10 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prisma";
+
+export const getAllPromptsRoute = async (app: FastifyInstance) => {
+  app.get("/prompt", async () => {
+    const prompts = await prisma.prompt.findMany();
+
+    return prompts;
+  });
+};
